f9864e681b351b4b4f97881f5d306b6fe33f3da2
Tom Wood <tomwood1875@gmail.com>
2025-05-14 14:35:48 +0100

    feat: add available borrowing calculation and clean up display

--- myscripts.js
+++ myscripts.js
@@ -5,14 +5,17 @@ calculateButton.addEventListener("click", () => {
     const depositAmount = Number(document.getElementById("deposit-amount").value);
     const annualIncome = Number(document.getElementById("annual-income").value);
 
-    console.log(housePrice);
-    console.log(depositAmount);
-    console.log(annualIncome);
-
     const requiredBorrowing = (housePrice - depositAmount);
 
-    const newContent = document.createTextNode("Required Borrowing: " + requiredBorrowing);
-    document.body.appendChild(newContent);
+    let requiredBorrowingParagraph = document.createElement("p");
+    requiredBorrowingParagraph.innerHTML = "Required Borrowing: " + requiredBorrowing;
+    document.body.appendChild(requiredBorrowingParagraph);
+
+    console.log(calculateAvailableBorrowing(annualIncome));
 });
 
+function calculateAvailableBorrowing(annualIncome) {
+    const availableBorrowing = (annualIncome * 4);
+    return availableBorrowing;
+}
 
