a30bc65661297e1d6a156116b23bac6c23b5273d
Tom Wood <tomwood1875@gmail.com>
2025-05-14 13:35:44 +0100

    feat: add calculation for required borrowing

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,18 @@
 const calculateButton = document.getElementById("calculate-btn");
 
 calculateButton.addEventListener("click", () => {
-    const newContent = document.createTextNode("Hi there and greetings!");
+    const housePrice = Number(document.getElementById("house-price").value);
+    const depositAmount = Number(document.getElementById("deposit-amount").value);
+    const annualIncome = Number(document.getElementById("annual-income").value);
+
+    console.log(housePrice);
+    console.log(depositAmount);
+    console.log(annualIncome);
+
+    const requiredBorrowing = (housePrice - depositAmount);
+
+    const newContent = document.createTextNode("Required Borrowing: " + requiredBorrowing);
     document.body.appendChild(newContent);
 });
+
+
