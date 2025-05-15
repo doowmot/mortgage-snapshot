72b53b69a7e5c1e6da92dc403731535d6325a00b
Tom Wood <tomwood1875@gmail.com>
2025-05-15 07:29:29 +0100

    feat: improve mortgage cost display with total and interest breakdown

--- myscripts.js
+++ myscripts.js
@@ -32,17 +32,21 @@ const calculateMonthlyCostBtn = document.getElementById("calculate-monthly-cost-
 calculateMonthlyCostBtn.addEventListener("click", () => {
     const annualInterestRate = Number(document.getElementById("annual-interest-rate").value);
     const mortgageTerm = Number(document.getElementById("mortgage-term").value);
-
     const housePrice = Number(document.getElementById("house-price").value);
     const depositAmount = Number(document.getElementById("deposit-amount").value);
 
     const requiredBorrowing = calculateRequiredBorrowing(housePrice, depositAmount);
-
     const monthlyCost = calculateMonthlyCost(requiredBorrowing, annualInterestRate, mortgageTerm);
+    const totalCost = calculateTotalCost(monthlyCost, mortgageTerm);
+    const totalInterestCost = totalCost - requiredBorrowing;
 
     const monthlyCostDisplay = document.createElement("p");
-    monthlyCostDisplay.innerHTML = `it will cost you £${monthlyCost} over a ${mortgageTerm} year mortgage term`;
+    monthlyCostDisplay.innerHTML = `You will pay £${monthlyCost} per month`;
     document.body.appendChild(monthlyCostDisplay);
+
+    const totalCostDisplay = document.createElement("p");
+    totalCostDisplay.innerHTML = `You will pay £${totalCost} over a ${mortgageTerm} year mortgage term (amount you borrowed: £${requiredBorrowing} + interest charged: £${totalInterestCost})`;
+    document.body.appendChild(totalCostDisplay);
 });
 
 function calculateMonthlyCost(requiredBorrowing, annualInterestRate, mortgageTerm) {
@@ -52,3 +56,6 @@ function calculateMonthlyCost(requiredBorrowing, annualInterestRate, mortgageTer
     return Math.round(requiredBorrowing * monthlyInterestRate * (Math.pow(1+monthlyInterestRate, numberOfPaymentMonths)) / (Math.pow(1+monthlyInterestRate, numberOfPaymentMonths) -1));
 }
 
+function calculateTotalCost(monthlyCost, mortgageTerm) {
+    return (monthlyCost * 12) * mortgageTerm;
+}
