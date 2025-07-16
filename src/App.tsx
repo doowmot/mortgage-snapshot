23ba2a952127d5a45b3ca578a5e39f657cc77b13
Tom Wood <tomwood1875@gmail.com>
2025-07-16 16:54:59 +0100

    Add basic mortgage borrowing form

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,29 @@ import './App.css'
 function App() {
 
   return (
-  <h1>test</h1>
+    <>
+      <h1>Mortgage Calculator</h1>
+
+      <div className="container">
+        <h2>How Much Can I Borrow?</h2>
+        <form id="borrowing-amount-form" onSubmit={(e) => e.preventDefault()}>
+          <label htmlFor="house-price">house price:</label>
+          <input type="text" id="house-price" name="house-price"/>
+          <br/>
+
+          <label htmlFor="deposit-amount">deposit amount:</label>
+          <input type="text" id="deposit-amount" name="deposit-amount"/>
+          <br/>
+
+          <label htmlFor="annual-income">annual income:</label>
+          <input type="text" id="annual-income" name="annual-income"/>
+          <br/>
+
+          <button id="calculate-borrowing-btn">Calculate borrowing</button>
+        </form>
+      </div>
+
+    </>
   )
 }
 
