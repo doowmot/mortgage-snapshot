fdcbecf1be155eb0d7bbba14ecbbc802f3fa5e82
Tom Wood <tomwood1875@gmail.com>
2025-07-17 11:29:31 +0100

    Add basic monthly cost form

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { useState } from "react";
 
 function App() {
   const [formData, setFormData] = useState({
-    housePrice: "",
     depositAmount: "",
     annualIncome: "",
-    result: "",
+    propertyValue: "",
+    interestRate: "",
+    mortgageTerm: "",
+    borrowingAmount: "",
   });
 
   const handleChange = (event) => {
@@ -17,38 +19,20 @@ function App() {
   const handleSubmit = (event) => {
     event.preventDefault();
     
-    let availableBorrowing = formData.annualIncome * 4;
-    console.log("available borrowing:", availableBorrowing)
+    let borrowingAmount = formData.annualIncome * 4;
 
-    let requiredBorrowing = formData.housePrice - formData.depositAmount;
-    console.log("required borrowing:", requiredBorrowing)
-
-    let result = formData.result
-
-    if (availableBorrowing >= requiredBorrowing) {
-        setFormData(prevState => ({
-            ...prevState,
-            result: `Great news! You can likely borrow £${availableBorrowing} which is more than you're required borrowing of £${requiredBorrowing}`
-        }));
-    } else {
-        setFormData(prevState => ({
-            ...prevState,
-            result: `Bad news! You require £${requiredBorrowing} but can only borrow £${availableBorrowing}`
-        }));
+    setFormData(prevState => ({
+        ...prevState,
+        borrowingAmount: `You can likely borrow £${borrowingAmount}`
+    }));
     }
-  }
+  
 
   return (
+    <>
+      <div>
+        <h2> How much can i borrow?</h2>
         <form onSubmit={handleSubmit}>
-          <label>
-            House Price:
-            <input 
-              type="number" 
-              name="housePrice"
-              value={formData.housePrice} 
-              onChange={handleChange}
-              />
-          </label>
           <label>
             Deposit Amount:
             <input 
@@ -65,9 +49,56 @@ function App() {
               value={formData.annualIncome} 
               onChange={handleChange}
             />
-          <input type="submit" />
-          <p>{formData.result}</p>
+          <input type="submit" value="calculate"/>
+          <p>{formData.borrowingAmount}</p>
         </form>
+      </div>
+
+      <div>
+        <h2> How much will it cost me?</h2>
+          <form onSubmit={handleSubmit}>
+          <label>
+            Deposit Amount:
+            <input 
+              type="number" 
+              name="depositAmount"
+              value={formData.depositAmount} 
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Property Value:
+            <input 
+              type="number" 
+              name="propertyValue"
+              value={formData.propertyValue} 
+              onChange={handleChange}
+              />
+          </label>
+          <label>
+            Interest Rate:
+            <input 
+              type="number" 
+              name="interestRate"
+              value={formData.interestRate} 
+              onChange={handleChange}
+              />
+          </label>
+          <label>
+            Mortgage Term:
+            <input 
+              type="number" 
+              name="mortgageTerm"
+              value={formData.mortgageTerm} 
+              onChange={handleChange}
+            />
+          </label>
+          <input type="submit" />
+          <p>{formData.monthlyCost}</p>
+        </form>
+        </div>
+    </>
+
   )
 }
 
