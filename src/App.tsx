6b7f252b327894b6d61f45f9d720a6a8e055dee4
Tom Wood <tomwood1875@gmail.com>
2025-08-21 21:27:07 +0100

    Add basic tailwind styling to affordability form

--- src/App.tsx
+++ src/App.tsx
@@ -243,30 +243,34 @@ function App() {
     
   return (
     <>
-      <div className="max-w-4xl mx-auto p-8">
-        <h1 className="text-3xl font-bold mb-8">Mortgage Calculator</h1>
+      <div className="p-8">
+        <h1 className="text-6xl font-bold">Mortgage Calculator</h1>
       </div>
-      
-      <div>
-      <h2 className="text-xl font-semibold mb-4">How much can I borrow?</h2>
+
+      <div className="p-8">
+      <h2 className="text-3xl font-semibold p-2">How much can I borrow?</h2>
         <form onSubmit={handleAffordabilitySubmit}>
-          <label>
-            Deposit Amount:
-            <input 
-              type="number" 
-              name="depositAmount"
-              value={formData.depositAmount} 
-              onChange={handleChange}
-            />
-          </label>
-            Annual Income:
-            <input 
-              type="number" 
-              name="annualIncome"
-              value={formData.annualIncome} 
-              onChange={handleChange}
-            />
-          <input type="submit" value="calculate"/>
+          <label className="block font-medium m-2">How much is your deposit?</label>
+          <input 
+            type="number" 
+            name="depositAmount"
+            value={formData.depositAmount} 
+            onChange={handleChange}
+            className="border-2 border-black rounded p-2 mb-4"
+          />
+          <label className="block font-medium m-2">What is your annual income?</label>
+          <input 
+            type="number" 
+            name="annualIncome"
+            value={formData.annualIncome} 
+            onChange={handleChange}
+            className="border-2 border-black rounded p-2 mb-4"
+          />
+          <input 
+            type="submit" 
+            value="calculate"
+            className="bg-blue-500 text-white px-4 py-2 rounded block mx-auto hover:bg-blue-400 cursor-pointer border-2 border-black"
+          />
           <p>{formData.borrowingAvailable}</p>
         </form>
       </div>
