a5dcb0601fb17b7ec0e76b9706121d0af69b19bf
Tom Wood <tomwood1875@gmail.com>
2025-08-29 09:01:22 +0100

    refactor affordability form into react component

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,39 @@ import {
 import { formatCurrency } from "./utils/format";
 declare const Chart: any;
 
+
+function AffordabilityForm(props) {
+  return (
+    <div className="p-8">
+      <h2 className="text-3xl font-semibold p-2">How much can I borrow?</h2>
+    <form onSubmit={props.handleAffordabilitySubmit}>
+      <label className="block font-medium m-2">How much is your deposit?</label>
+      <input 
+        type="number" 
+        name="depositAmount"
+        value={props.depositAmount} 
+        onChange={props.handleChange}
+        className="border-2 border-black rounded p-2 mb-4"
+      />
+      <label className="block font-medium m-2">What is your annual income?</label>
+      <input 
+        type="number" 
+        name="annualIncome"
+        value={props.annualIncome} 
+        onChange={props.handleChange}
+        className="border-2 border-black rounded p-2 mb-4"
+      />
+      <input 
+        type="submit" 
+        value="calculate"
+        className="bg-blue-500 text-white px-4 py-2 rounded block mx-auto hover:bg-blue-400 cursor-pointer border-2 border-black"
+      />
+      <p className="text-2xl p-10 font-extrabold">{props.borrowingAvailable}</p>
+    </form>
+    </div>
+  );
+}
+
 function App() {
   const [formData, setFormData] = useState({
     depositAmount: "25000",
@@ -185,33 +218,13 @@ function App() {
         <h1 className="text-6xl font-bold">Mortgage Calculator</h1>
       </div>
 
-      <div className="p-8">
-      <h2 className="text-3xl font-semibold p-2">How much can I borrow?</h2>
-        <form onSubmit={handleAffordabilitySubmit}>
-          <label className="block font-medium m-2">How much is your deposit?</label>
-          <input 
-            type="number" 
-            name="depositAmount"
-            value={formData.depositAmount} 
-            onChange={handleChange}
-            className="border-2 border-black rounded p-2 mb-4"
-          />
-          <label className="block font-medium m-2">What is your annual income?</label>
-          <input 
-            type="number" 
-            name="annualIncome"
-            value={formData.annualIncome} 
-            onChange={handleChange}
-            className="border-2 border-black rounded p-2 mb-4"
-          />
-          <input 
-            type="submit" 
-            value="calculate"
-            className="bg-blue-500 text-white px-4 py-2 rounded block mx-auto hover:bg-blue-400 cursor-pointer border-2 border-black"
-          />
-          <p className="text-2xl p-10 font-extrabold">{formData.borrowingAvailable}</p>
-        </form>
-      </div>
+      <AffordabilityForm 
+        handleAffordabilitySubmit={handleAffordabilitySubmit}
+        handleChange={handleChange}
+        depositAmount={formData.depositAmount}
+        annualIncome={formData.annualIncome}
+        borrowingAvailable={formData.borrowingAvailable}
+      />
 
       {showCostsForm &&
       <div className="p-8">
