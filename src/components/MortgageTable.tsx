951c799fe454be3ba8892ea01844db1eaba33505
Tom Wood <tomwood1875@gmail.com>
2025-09-02 16:20:01 +0100

    add css to mortgage table

--- src/components/MortgageTable.tsx
+++ src/components/MortgageTable.tsx
@@ -2,25 +2,25 @@ import { formatCurrency } from "../utils/format";
 
 export function MortgageTable(props) {
     return (
-        <div>
-        <table>
-        <tbody>
-        <tr>
-            <th>Year</th>
-            <th>Interest</th>
-            <th>Capital</th>
-            <th>Balance Remaining</th>
-        </tr>
-        {props.amortisationSchedule.map((item) => (
-            <tr key={item.year}>
-            <td>{item.year}</td>
-            <td>{formatCurrency(item.interest)}</td>
-            <td>{formatCurrency(item.capital)}</td>
-            <td>{formatCurrency(item.balance)}</td>
+      <div className="p-8">
+        <table className="w-full table-fixed border-2 border-black">
+          <tbody>
+            <tr className="bg-blue-500 text-white">
+              <th className="border-2 border-black p-3 font-semibold text-center w-1/4">Year</th>
+              <th className="border-2 border-black p-3 font-semibold text-center w-1/4">Interest</th>
+              <th className="border-2 border-black p-3 font-semibold text-center w-1/4">Capital</th>
+              <th className="border-2 border-black p-3 font-semibold text-center w-1/4">Balance Remaining</th>
             </tr>
-        ))}
-        </tbody>
+            {props.amortisationSchedule.map((item) => (
+              <tr key={item.year} className="hover:bg-gray-50">
+                <td className="border-2 border-black p-3 text-center">{item.year}</td>
+                <td className="border-2 border-black p-3 text-center">{formatCurrency(item.interest)}</td>
+                <td className="border-2 border-black p-3 text-center">{formatCurrency(item.capital)}</td>
+                <td className="border-2 border-black p-3 text-center">{formatCurrency(item.balance)}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
-        </div>
+      </div>
     )
-}
+  }
