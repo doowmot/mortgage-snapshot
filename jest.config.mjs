01261a253a48a13be3bfaa6719b189972717c643
Tom Wood <tomwood1875@gmail.com>
2025-09-04 09:58:47 +0100

    add jest config for typescript

---
+++ jest.config.mjs
@@ -0,0 +1,12 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  }
+};
