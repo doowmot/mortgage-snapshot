c00b931b7ae9e9b8f17750988f8b3913ae03b0de
Tom Wood <tomwood1875@gmail.com>
2026-04-02 10:30:37 +0100

    add cumulative columns and milestone highlighting to mortgage table

--- src/components/MortgageTable.tsx
+++ src/components/MortgageTable.tsx
@@ -2,16 +2,19 @@ import { formatCurrency } from "../utils/format";
 
 export function MortgageTable(props) {
     let inflectionFound = false;
+    let milestoneFound = false;
 
     return (
       <div className="p-8">
         <table className="w-full table-fixed border-2 border-black">
           <tbody>
             <tr className="bg-blue-500 text-white">
-              <th className="border-2 border-black p-3 font-semibold text-center w-1/4">Year</th>
-              <th className="border-2 border-black p-3 font-semibold text-center w-1/4">Interest</th>
-              <th className="border-2 border-black p-3 font-semibold text-center w-1/4">Capital</th>
-              <th className="border-2 border-black p-3 font-semibold text-center w-1/4">Balance</th>
+              <th className="border-2 border-black p-3 font-semibold text-center w-1/6">Year</th>
+              <th className="border-2 border-black p-3 font-semibold text-center w-1/6">Interest</th>
+              <th className="border-2 border-black p-3 font-semibold text-center w-1/6">Capital</th>
+              <th className="border-2 border-black p-3 font-semibold text-center w-1/6">Balance</th>
+              <th className="border-2 border-black p-3 font-semibold text-center w-1/6">Cumulative Interest</th>
+              <th className="border-2 border-black p-3 font-semibold text-center w-1/6">Cumulative Capital</th>
             </tr>
             {props.yearlyAmortisationSchedule
               .filter((item) => item.year !== 0)
@@ -19,12 +22,21 @@ export function MortgageTable(props) {
                 const isInflection = !inflectionFound && item.capital > item.interest;
                 if (isInflection) inflectionFound = true;
 
+                const isMilestone = !milestoneFound && item.cumulativeCapital > item.cumulativeInterest;
+                if (isMilestone) milestoneFound = true;
+
+                let rowClassName = "hover:bg-gray-50";
+                if (isInflection) rowClassName = "bg-yellow-200";
+                if (isMilestone) rowClassName = "bg-blue-200";
+
                 return (
-                  <tr key={item.year} className={isInflection ? "bg-yellow-200" : "hover:bg-gray-50"}>
+                  <tr key={item.year} className={rowClassName}>
                     <td className="border-2 border-black p-3 text-center">{item.year}</td>
                     <td className="border-2 border-black p-3 text-center text-red-600">{formatCurrency(item.interest)}</td>
                     <td className="border-2 border-black p-3 text-center text-green-600">{formatCurrency(item.capital)}</td>
                     <td className="border-2 border-black p-3 text-center">{formatCurrency(item.balance)}</td>
+                    <td className="border-2 border-black p-3 text-center text-red-600">{formatCurrency(item.cumulativeInterest)}</td>
+                    <td className="border-2 border-black p-3 text-center text-green-600">{formatCurrency(item.cumulativeCapital)}</td>
                   </tr>
                 );
               })}
