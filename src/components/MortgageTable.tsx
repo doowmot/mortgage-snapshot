f0ffd17eb57859fb4b9be7f1644783683dcb05da
Tom Wood <tomwood1875@gmail.com>
2026-04-02 09:42:57 +0100

    add inflection point highlight and colour coding to mortgage table

--- src/components/MortgageTable.tsx
+++ src/components/MortgageTable.tsx
@@ -1,6 +1,8 @@
 import { formatCurrency } from "../utils/format";
 
 export function MortgageTable(props) {
+    let inflectionFound = false;
+
     return (
       <div className="p-8">
         <table className="w-full table-fixed border-2 border-black">
@@ -9,16 +11,23 @@ export function MortgageTable(props) {
               <th className="border-2 border-black p-3 font-semibold text-center w-1/4">Year</th>
               <th className="border-2 border-black p-3 font-semibold text-center w-1/4">Interest</th>
               <th className="border-2 border-black p-3 font-semibold text-center w-1/4">Capital</th>
-              <th className="border-2 border-black p-3 font-semibold text-center w-1/4">Balance Remaining</th>
+              <th className="border-2 border-black p-3 font-semibold text-center w-1/4">Balance</th>
             </tr>
-            {props.yearlyAmortisationSchedule.map((item) => (
-              <tr key={item.year} className="hover:bg-gray-50">
-                <td className="border-2 border-black p-3 text-center">{item.year}</td>
-                <td className="border-2 border-black p-3 text-center">{formatCurrency(item.interest)}</td>
-                <td className="border-2 border-black p-3 text-center">{formatCurrency(item.capital)}</td>
-                <td className="border-2 border-black p-3 text-center">{formatCurrency(item.balance)}</td>
-              </tr>
-            ))}
+            {props.yearlyAmortisationSchedule
+              .filter((item) => item.year !== 0)
+              .map((item) => {
+                const isInflection = !inflectionFound && item.capital > item.interest;
+                if (isInflection) inflectionFound = true;
+
+                return (
+                  <tr key={item.year} className={isInflection ? "bg-yellow-200" : "hover:bg-gray-50"}>
+                    <td className="border-2 border-black p-3 text-center">{item.year}</td>
+                    <td className="border-2 border-black p-3 text-center text-red-600">{formatCurrency(item.interest)}</td>
+                    <td className="border-2 border-black p-3 text-center text-green-600">{formatCurrency(item.capital)}</td>
+                    <td className="border-2 border-black p-3 text-center">{formatCurrency(item.balance)}</td>
+                  </tr>
+                );
+              })}
           </tbody>
         </table>
       </div>
