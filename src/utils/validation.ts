c1ff837214b6c523c5cc303b159e1a0f567fa3c2
Tom Wood <tomwood1875@gmail.com>
2026-04-10 13:04:50 +0100

    Add TypeScript typing to validation.ts

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,48 +1,56 @@
-export const validateDeposit = (deposit) => {
+export const validateDeposit = (deposit: string | number): string | null => {
   if (isNaN(Number(deposit))) {
     return "Error: Please enter a number for the deposit";
   } else if (Number(deposit) <= 0) {
-  return "Error: Please ensure the deposit is greater than 0";
-  } 
+    return "Error: Please ensure the deposit is greater than 0";
+  }
+
   return null;
-}
-  
-export const validateIncome = (income) => {
+};
+
+export const validateIncome = (income: string | number): string | null => {
   if (isNaN(Number(income))) {
     return "Error: Please enter a number for the income";
   } else if (Number(income) <= 0) {
-  return "Error: Please ensure income is greater than 0";
-  } 
-  return null;
-}
+    return "Error: Please ensure income is greater than 0";
+  }
 
-export const validatePropertyPrice = (deposit, housePrice) => {
+  return null;
+};
+
+export const validatePropertyPrice = (
+  deposit: string | number,
+  housePrice: string | number,
+): string | null => {
   if (isNaN(Number(housePrice))) {
     return "Error: Please enter a number for the house price";
   } else if (Number(housePrice) <= 0) {
     return "Error: Please ensure property price is greater than 0";
-  } else if (Number(housePrice) < deposit) {
+  } else if (Number(housePrice) < Number(deposit)) {
     return "Error: Please ensure property price is greater than the deposit";
-  } 
-  return null;
-}
+  }
 
-export const validateInterestRate = (annualRate) => {
+  return null;
+};
+
+export const validateInterestRate = (annualRate: string | number): string | null => {
   if (isNaN(Number(annualRate))) {
     return "Error: Please enter a number for the interest rate";
   } else if (Number(annualRate) <= 0) {
-  return "Error: Please ensure interest rate is greater than 0";
-  } 
-  return null;
-}
+    return "Error: Please ensure interest rate is greater than 0";
+  }
 
-export const validateMortgageTerm = (mortgageYears) => {
+  return null;
+};
+
+export const validateMortgageTerm = (mortgageYears: string | number): string | null => {
   if (isNaN(Number(mortgageYears))) {
     return "Error: Please enter a number for the mortgage term";
   } else if (Number(mortgageYears) <= 0) {
-  return "Error: Please ensure mortgage term is greater than 0";
+    return "Error: Please ensure mortgage term is greater than 0";
   } else if (Number(mortgageYears) > 40) {
-  return "Error: Please ensure mortgage term is 40 years or less";
+    return "Error: Please ensure mortgage term is 40 years or less";
   }
+
   return null;
-}
+};
